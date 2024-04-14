e0f740e4f2e6be5b49179186dd08d95b51ea43f3
Yipmong Said <Syipmong5@gmail.com>
2024-04-15 00:04:57 +0100

    ensuring layout works

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,7 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>
-      <nav className="flex justify-between p-4 bg-blue-500 text-white">
-        <div>Logo</div>
-        <div className="space-x-4">
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
-        </div>
-        </nav>
+      <Navbar/>
         {children}
         </body>
     </html>
